13acd430a06d0f946d1ad925ec2283dcec6479b3
sager <kingsager404@gmail.com>
2023-03-22 22:09:20 +0800

    fix uclamp

--- oplus/kernel/oplus_performance/sched_assist/sched_assist_common.c
+++ oplus/kernel/oplus_performance/sched_assist/sched_assist_common.c
@@ -247,11 +247,10 @@ bool should_force_spread_tasks(void) {
 #ifdef CONFIG_CGROUP_SCHED
 static inline int task_cgroup_id(struct task_struct *task)
 {
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 0))
+
 	struct cgroup_subsys_state *css = task_css(task, cpu_cgrp_id);
-#else
-	struct cgroup_subsys_state *css = task_css(task, schedtune_cgrp_id);
-#endif
+
+
 
 	return css ? css->id : -1;
 }
@@ -1620,11 +1619,9 @@ int get_grp(struct task_struct *p)
 	if (p == NULL)
 		return false;
 	rcu_read_lock();
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 0))
+
 	css = task_css(p, cpu_cgrp_id);
-#else
-	css = task_css(p, schedtune_cgrp_id);
-#endif
+
 	if (!css) {
 		rcu_read_unlock();
 		return false;
